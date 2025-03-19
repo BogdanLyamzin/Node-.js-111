a618990873fd854de61cd1582d6eedbe775d710c
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-03-19 20:56:51 +0100

    finish lesson 2

---
+++ lesson-2/nodejs-hw-01-template/src/constants/songs.js
@@ -0,0 +1,4 @@
+import path from "node:path";
+
+// export const SONG_DB_PATH = path.join(process.cwd(), "src", "db", "db-songs.json");
+export const SONG_DB_PATH = path.resolve("src", "db", "db-songs.json");
