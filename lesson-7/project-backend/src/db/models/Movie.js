be4e8bd672cf1078dac777056c50337391bee583
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-07 20:05:10 +0200

    finish lesson 7

---
+++ lesson-7/project-backend/src/db/models/Movie.js
@@ -0,0 +1,37 @@
+import {Schema, model} from "mongoose";
+
+import { typeList } from "../../constants/movies.js";
+
+import { handleSaveError, setUpdateSettings } from "./hooks.js";
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, "Назва фільма обов'язкова"],
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: typeList,
+        default: typeList[0],
+        required: true,
+    }
+}, {versionKey: false, timestamps: true});
+
+movieSchema.post("save", handleSaveError);
+
+movieSchema.pre("findOneAndUpdate", setUpdateSettings);
+
+movieSchema.post("findOneAndUpdate", handleSaveError);
+
+const MovieCollection = model("movie", movieSchema);
+
+export default MovieCollection;
---
+++ lesson-7/project-backend/src/validation/movies.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+import { typeList } from "../constants/movies.js";
+
+export const movieAddSchema = Joi.object({
+    title: Joi.string().required().messages({
+        "any.required": "Треба вказати назву фільму",
+        "string.base": "Назва фильму має бути строкою"
+    }),
+    director: Joi.string().required(),
+    favorite: Joi.boolean(),
+    type: Joi.string().valid(...typeList),
+});
+
+export const movieUpdateSchema = Joi.object({
+    title: Joi.string(),
+    director: Joi.string(),
+    favorite: Joi.boolean(),
+    type: Joi.string().valid(...typeList),
+});
